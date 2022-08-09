d59e9b1a13d9710b13087a5b4682c2ca4d22e705
kalhararajapaksha <kasunkrajapaksha@gmail.com>
2022-08-10 00:44:53 +0530

    Setup Hotel management backend

---
+++ Back-End/HotelMngtAPI/HotelMngtAPI/IServiceUser.cs
@@ -0,0 +1,33 @@
+﻿using HotelMngt.REF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace HotelMngtAPI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceUser" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceUser
+    {
+     [OperationContract]
+     [WebInvoke(Method = "GET",
+     UriTemplate = "LoadAllUsers",
+     BodyStyle = WebMessageBodyStyle.Wrapped,
+     RequestFormat = WebMessageFormat.Json,
+     ResponseFormat = WebMessageFormat.Json)]
+      string LoadAllUsers();
+
+
+     [OperationContract]
+     [WebInvoke(Method = "POST",
+     UriTemplate = "SaveUser",
+     BodyStyle = WebMessageBodyStyle.Wrapped,
+     RequestFormat = WebMessageFormat.Json,
+     ResponseFormat = WebMessageFormat.Json)]
+     string SaveUser(REF_User oREF_User);
+    }
+}
