f879615ef04317af6ad6eb0f96bd82c5cd4049a3
kalhararajapaksha <kasunkrajapaksha@gmail.com>
2022-08-10 22:25:34 +0530

    Create user login APIs

--- Back-End/HotelMngtAPI/HotelMngtAPI/ServiceUser.svc.cs
+++ Back-End/HotelMngtAPI/HotelMngtAPI/ServiceUser.svc.cs
@@ -16,6 +16,29 @@ namespace HotelMngtAPI
     // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceUser.svc or ServiceUser.svc.cs at the Solution Explorer and start debugging.
     public class ServiceUser : IServiceUser
     {
+        public string GetUserByUserName(REF_User oREF_User)
+        {
+            try
+            {
+                BAL_User oBAL_User = new BAL_User();
+
+
+                DataTable dt;
+                dt = oBAL_User.SelectByUserNameAndPassword(oREF_User, null);
+              //  Response oResponse = new Response();
+               // oResponse.Success = true;
+                //oResponse.oDataTable = dt;
+                return JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex)
+            {
+               // Response oResponse = new Response();
+              //  oResponse.Success = false;
+               // oResponse.Message = "Error! Something went wrong. Please contact the system administrator. (" + ex.Message + ")";
+                return JsonConvert.SerializeObject("Error! Something went wrong. Please contact the system administrator.");
+            }
+        }
+
         public string LoadAllUsers()
         {
             DataTable dt = new DataTable();
